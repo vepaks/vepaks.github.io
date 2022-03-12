a0bdc85a2ede61c3bd0175348cd3f46f129410bb
Aleksandar Kamburov <kamburoff@gmail.com>
2022-03-12 09:30:17 +0200

    12.03.2022

--- js/script.js
+++ js/script.js
@@ -5,28 +5,39 @@ const codeList = {
         '<script src="https://gist.github.com/vepaks/d25560bc5d81a94b6597bc7ca14c0cdf.js"></script>',
         '<script src="https://gist.github.com/vepaks/d25560bc5d81a94b6597bc7ca14c0cdf.js"></script>',
         '<script src="https://gist.github.com/vepaks/d25560bc5d81a94b6597bc7ca14c0cdf.js"></script>',
+        '<script src="https://gist.github.com/vepaks/d25560bc5d81a94b6597bc7ca14c0cdf.js"></script>',
     ]
 };
+
+
+const listOfEntry = {
+    list: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
+};
+
 
 let entryModule = document.getElementById('entry_module');
 let simpleText = document.querySelector('.header-discription');
 let listOfModule = document.querySelectorAll('.list-group-item');
 
-
-listOfModule.forEach(item => {
-    console.log(item);
-});
+listOfModule.innerHTML = "";
 
 function hover() {
-    listOfModule.forEach(item => {
-        item.remove();
+
+    listOfEntry.list.forEach((film, i) => {
+        listOfModule.innerHTML += `
+            <li class="promo__interactive-item">${i+1}. ${film}
+                <div class="delete"></div>
+            </li>
+        `;
     });
 
-    // simpleText.textContent = 'OK!';
 }
 
 
 entryModule.addEventListener('click', hover);
-
-
-
