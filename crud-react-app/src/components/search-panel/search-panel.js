53364dac15781844fdc849f67e85662d6ba16442
kamburoff <kamburoff@gmail.com>
2023-04-05 09:34:49 +0300

    scaffold crud-react-app

---
+++ crud-react-app/src/components/search-panel/search-panel.js
@@ -0,0 +1,11 @@
+const SearchPanel = () => {
+  return (
+    <input
+      type="text"
+      className="form-control search-input"
+      placeholder="Намети служител"
+    />
+  );
+};
+
+export default SearchPanel;
